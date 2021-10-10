eb196dad369a395728ec7ac2056f6ac7e5b19581
Athul <athulsprakash@gmail.com>
2021-10-10 20:31:18 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,140 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+//import { calculate } from "./components/calculate.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+const App = () => {
+
+  const [result, setResult] = useState("");
+  const [calc, setCalc] = useState("");
+  const ops = ["/", "*", "-", "+", ".", "%"];
+
+  const updateCalc = (value) => {
+    if (
+      (ops.includes(value) && calc === "") ||
+      (ops.includes(value) && ops.includes(calc.slice(-1)))
+    ) {
+      return;
+    }
+    if(value==='0' && calc.slice(0)==='0'){
+      return;
+    }
+    setCalc(calc + value);
+    document.querySelector('#input').classList.remove('result');
+    if (!ops.includes(value)) {
+      setResult(eval(calc + value).toString());
+    }
+  };
+
+  const calculator = () => {
+    setCalc(result);
+    setResult('');
+    document.querySelector('#input').classList.add('result');
+  };
+
+  const clear = () => {
+    setCalc("");
+    setResult("");
+    document.querySelector('#input').classList.remove('result');
+  };
+
+  const backspace = () => {
+    if (calc === "") {
+      return;
+    }
+    const value = calc.slice(0, -1);
+    setCalc(value);
+    document.querySelector('#input').classList.remove('result');
+  };
+
+  const createDigits = () => {
+    const digits = [];
+    for (let i = 1; i < 10; i++) {
+      digits.push(
+        <button
+          className="pad-item"
+          onClick={() => updateCalc(i.toString())}
+          key={i}
         >
-          Learn React
-        </a>
-      </header>
+          {i}
+        </button>
+      );
+    }
+    return digits;
+  };
+
+  return (
+    <div className="pad">
+      <div className="pad-item display">
+        <span id="output">{result ? '('+ result +')': ''}</span> 
+        <span id="input">{calc || "0"}</span>
+      </div>
+      <div className="misc">
+        <button className="pad-item" onClick={() => updateCalc("^(2)")}>
+          <div>
+            x
+            <sup>
+              <small>2</small>
+            </sup>
+          </div>
+        </button>
+        <button className="pad-item" onClick={() => updateCalc("^(3)")}>
+          <div>
+            x
+            <sup>
+              <small>3</small>
+            </sup>
+          </div>
+        </button>
+        <button className="pad-item" onClick={() => updateCalc("^")}>
+          <div>
+            x
+            <sup>
+              <small>y</small>
+            </sup>
+          </div>
+        </button>
+        <button className="pad-item"> &pi; </button>
+      </div>
+      <div className="ctrl">
+        <button className="pad-item" id="clear" onClick={() => clear()}>
+          C
+        </button>
+        <button className="pad-item" onClick={() => updateCalc("/")}>
+          /
+        </button>
+        <button className="pad-item" onClick={() => updateCalc("*")}>
+          &times;
+        </button>
+        <button className="pad-item" onClick={() => backspace()}>
+          <i className="fas fa-backspace"></i>
+        </button>
+      </div>
+      <div className="operators">
+        <button className="pad-item" onClick={() => updateCalc("-")}>
+          -
+        </button>
+        <button className="pad-item" onClick={() => updateCalc("+")}>
+          +
+        </button>
+        <button className="pad-item" id="equal" onClick={() => calculator()}>
+          =
+        </button>
+      </div>
+
+      <div className="digits">
+        {createDigits()}
+        <button className="pad-item" onClick={() => updateCalc("%")}>
+          %
+        </button>
+        <button className="pad-item" onClick={() => updateCalc("0")}>
+          0
+        </button>
+        <button className="pad-item" onClick={() => updateCalc(".")}>
+          .
+        </button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
